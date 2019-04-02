de083bdf2ca46488db360cb8202570e2c050dc52
Michał Jaworski <swistakm@gmail.com>
2019-04-02 22:25:13 +0200

    chapter1 scripts

---
+++ chapter1/NOTES.md
@@ -0,0 +1,4 @@
+Map of example scripts to Chapter sections
+
+* "The popular tools and techniques used for maintaining cross-version compatibility"
+  * `example_compat.py`
---
+++ chapter1/example_compat.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""This module provides compatibility layer for
+selected things that have changed across Python
+versions.
+"""
+import sys
+
+if sys.version_info < (3, 0, 0):
+    import urlparse  # noqa
+
+
+    def is_string(s):
+        """Return True if given value is considered string"""
+        return isinstance(s, basestring)
+
+else:
+    # note: urlparse was moved to urllib.parse in Python 3
+    from urllib import parse as urlparse  # noqa
+
+
+    def is_string(s):
+        """Return True if given value is considered string"""
+        return isinstance(s, str)
